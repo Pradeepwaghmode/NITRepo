aeb2e748d9759197cf97a8f1fa6af79b0088d190
PL <PL@gmail.com>
2023-01-04 12:48:39 +0530

    request parameter binding

--- BootMVCProj06-DataBinding-RequestParamBinding/src/main/java/com/nt/controller/StudentOperations.java
+++ BootMVCProj06-DataBinding-RequestParamBinding/src/main/java/com/nt/controller/StudentOperations.java
@@ -1,6 +1,10 @@
 package com.nt.controller;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +19,30 @@ public class StudentOperations {
 		return "welcome";
 	}*/
 	
-	@GetMapping("/data")
-	public String showHomePage(@RequestParam int sno,@RequestParam(required = false , defaultValue = "rrr") String sname){
+	/*@GetMapping("/data")
+	public String showHomePage(@RequestParam(defaultValue = "12") int sno,@RequestParam(required = false , defaultValue = "rrr") String sname){
 		System.out.println(sno +"   "+sname);
+		return "welcome";
+	}
+	*/
+	
+	/*@GetMapping("/data")
+	public String showHomePage(@RequestParam(defaultValue = "12") int sno,@RequestParam(required = false , defaultValue = "rrr") String sname,
+			@RequestParam(name="sadd")String sadd[],
+			@RequestParam(name="sadd")Set<String> saddSet,
+			@RequestParam(name="sadd")List<String> saddList){
+		System.out.println(sno +"   "+sname+" "+Arrays.toString(sadd)+" ");
+		System.out.println("List:"+saddList);
+		System.out.println("set:"+saddSet);
+		return "welcome";
+	}*/
+	
+	@GetMapping("/data")
+	public String showHomePage(@RequestParam(defaultValue = "12") int sno,@RequestParam(required = false , defaultValue = "rrr") String sname,
+			@RequestParam(name="sadd")String sadd
+		){
+		System.out.println(sno +"   "+sname+" "+sadd+" ");
+		
 		return "welcome";
 	}
 
--- BootMVCProj06-DataBinding-RequestParamBinding/src/main/webapp/WEB-INF/pages/welcome.jsp
+++ BootMVCProj06-DataBinding-RequestParamBinding/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -5,7 +5,7 @@
 <h1 style="color: fuchsia;text-align: center;border: thick;">Param Values</h1><br>
 <h1 style="color: fuchsia;text-align: center;border: thick;"> ${param.sno}</h1><br>
 <h1 style="color: fuchsia;text-align: center;border: thick;"> ${param.sname}</h1><br>
-
+<h1 style="color: fuchsia;text-align: center;border: thick;"> ${param.sadd}</h1><br>
 
 
 
